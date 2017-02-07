6c5fd2fc8ddfbda7921961ea291b8ea148cb63cb
Joel Ross <joelross@uw.edu>
2017-02-06 20:40:07 -0800

    Add starter version

--- index.ts
+++ index.ts
@@ -1,84 +1,136 @@
-import {TTTGame as Game, Observer} from "./model";
-import * as $ from 'jquery';
+//for CLI interactiv
+const readline = require('readline');
+const io = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout
+});
 
-class View {
-  private playerSymbols = [' ','X','O'];
-  constructor(private game:Game, private ctrl:Controller){}
+/**
+ * Represents a game of Tic Tac Toe.
+ * Board size is hard-coded at 3.
+ */
+export class TTTGame {
+  private playerSymbols = [' ','X','O']; //for display
+  private gameBoard:number[][];
+  private currentPlayer:number = 0;
+  private winner:number = undefined;
+  public readonly size = 3; //hard-coded for simplicity
 
-  display() {
-    console.log("displaying...");
+  constructor() {
+    this.resetBoard(); //initialize board
+  }
 
-    let gamebox = $('#game-box');
-    gamebox.empty(); //clear old display
+  resetBoard() {
+    this.gameBoard = [
+      [undefined, undefined, undefined],
+      [undefined, undefined, undefined],
+      [undefined, undefined, undefined],
+    ];
+  }
 
-    // make grid of buttons
-    for(let i=0; i<this.game.size; i++){ //row
-      let row = $('<div>'); //a row for the button
-      for(let j=0; j<this.game.size; j++) {
-        let player = this.game.getPiece(i,j);
+  //returns if sucessful or not
+  makeMove(x:number, y:number):boolean{ 
+    if(this.winner) return false; //don't move if won
+    if(x <0 || x > 2 || y < 0 || y > 2) return false; //out of bounds
+    if(this.gameBoard[x][y] !== undefined) return false; //don't move if illegal
+
+    this.gameBoard[x][y] = this.currentPlayer; //make move
+
+    //check if we now have a winner
+    let gb = this.gameBoard;
+
+    //check row
+    if(gb[x][0] == gb[x][1] && gb[x][1] == gb[x][2]) this.winner = this.currentPlayer; 
+
+    //check col
+    if(gb[0][y] == gb[1][y] && gb[1][y] == gb[2][y]) this.winner = this.currentPlayer;
+
+    //check diag
+    if( gb[1][1] !== undefined && (
+        (gb[0][0] == gb[1][1] && gb[1][1] == gb[2][2]) || 
+        (gb[2][0] == gb[1][1] && gb[1][1] == gb[0][2]) )) 
+      this.winner = this.currentPlayer
+
+    this.currentPlayer = (this.currentPlayer+1) % 2; //toggle
+
+    return true;
+  }
+
+  getPiece(x:number, y:number):number{
+    if(x <0 || x > 2 || y < 0 || y > 2) return undefined; //out of bounds
+    return this.gameBoard[x][y];
+  }
+
+  getBoard() {
+    return this.gameBoard;
+  }
+  
+  getCurrentPlayer():number {
+    return this.currentPlayer;
+  }
+
+  getWinner():number {
+    return this.winner;
+  }
+
+  //starts the game
+  play() {
+    this.printBoard();
+    this.takeTurn();    
+  }
+
+  takeTurn() {
+    this.printPrompt();
+    io.question('> ', (input) => {
+      try {
+        let cell = input.split(',');
+        //make a move!
+        let result = this.makeMove(Number(cell[0]),Number(cell[1]));
+        if(result){ //legal move
+          this.printBoard();
+          if(this.getWinner() !== undefined){
+            this.printWinner(this.getWinner());
+            io.close();
+            return; //end
+          }
+        }
+      } catch(e) {} //for parsing errors
+
+      this.takeTurn(); //recurse!
+    })
+  }
+
+  printBoard() {
+    //print the board
+    console.log("    0   1   2")
+    for(let i=0; i<this.size; i++) {
+      let row = i+"   ";
+      for(let j=0; j<this.size; j++) {
+
+        let player = this.getPiece(i,j);
         if(player === undefined) player = -1;
+        row += this.playerSymbols[player+1];
 
-        let button = $('<button class="btn btn-default">'+this.playerSymbols[player+1]+'</button>')
-        button.click((e) => this.handleClick(i,j)); //closure!!
-        row.append(button);
+        if(j < this.size - 1) 
+          row += " | ";
       }
-      gamebox.append(row);
+      console.log(row);
+      if(i < this.size -1)
+        console.log("   -----------");
     }
-
-    //show winner, if any
-    let winner = game.getWinner();
-    if(winner !== undefined){
-      this.showWinner(winner)
-      $('button').attr('disabled','disabled'); //disable all the buttons
-    }
-    else {
-      this.showPrompt(); //show prompt for next move
-    }
+    console.log("");
   }
 
-  //callback for clicking
-  handleClick(row, col) {
-    this.ctrl.takeTurn(row, col); //tell controller what to do
-  }
-
-  showPrompt():void {
+  printPrompt() {
     let player = this.playerSymbols[game.getCurrentPlayer()+1]
-    $('#message').html('<p class="lead">'+player+"'s turn. Pick a spot!"+'</p>')
+    console.log(player+"'s turn. Pick a spot [row, col]");
   }
 
-  showWinner(winner:number):void {
+  printWinner(winner:number):void {
     let player = this.playerSymbols[winner+1]
-    $('#message').html('<p class="lead">'+player+" is the winner!"+'</p>')
-  }
-
-  //observer
-  notify() {
-    this.display();
+    console.log(player+" is the winner!");
   }
 }
 
-class Controller {
-  private view:View;
-  constructor(private game:Game){
-    this.view = new View(game, this);
-    this.game.register(this.view); //connect game and view
-    this.view.display(); //show the initial view
-  }
-
-  //when told what to do, pass it along to the game
-  takeTurn(row:number, col:number):void{
-    if(this.game.getWinner() === undefined)
-      this.game.makeMove(row, col);
-  }
-}
-
-//run the program!
-let game:Game = new Game();
-
-// game.makeMove(0,0); //x //debugging
-// game.makeMove(0,1); //o
-// game.makeMove(1,0); //x
-// game.makeMove(1,1); //o
-// game.makeMove(2,0); //x
-
-let ctrl:Controller = new Controller(game); //will create the View
+let game:TTTGame = new TTTGame();
+game.play();
